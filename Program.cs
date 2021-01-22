3098dcc1e9251d497b0b0664b1c49336c71216bc
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-01-21 17:18:32 -0700

    Add files via upload
    Add project 4

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CS133PROJECT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake<string, string> myCake = new Cake<string, string>();
+
+            myCake.Flavor = "Vanilla";
+            myCake.Type = "Wedding Cake";
+
+            Cake<string, string> myCake2 = new Cake<string, string>();
+
+            myCake2.Flavor = "Chocolate";
+            myCake2.Type = "Party Cake";
+
+
+            Console.WriteLine("My first Cake is for: " + myCake.Type); 
+
+            Console.WriteLine("My flavor for this cake is: " +  myCake.Flavor);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("My second Cake is for: " + myCake2.Type);
+
+            Console.WriteLine("My flavor for this cake is: " + myCake2.Flavor);
+            
+        }
+    }
+}
+
+public class Cake<C, W>
+{
+    public C Flavor { get; set; }
+    public W Type { get; set; }
+}
